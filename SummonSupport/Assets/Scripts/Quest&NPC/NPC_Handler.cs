7f4e8b8bf757887961aaf66893cf012a50dcca85
Coarse-Biologist <168189263+Coarse-Biologist@users.noreply.github.com>
2025-04-08 16:10:59 -0700

    working on flow of NPC dialogue interaction

--- SummonSupport/Assets/Scripts/Quest&NPC/NPC_Handler.cs
+++ SummonSupport/Assets/Scripts/Quest&NPC/NPC_Handler.cs
@@ -1,10 +1,14 @@
+using TMPro;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC_Handler : MonoBehaviour, I_Interactable
 {
     [SerializeField] NPC_SO npcData;
     [SerializeField] GameObject interactCanvas;
     private GameObject canvasInstance;
+    private TextMeshProUGUI dialogue;
 
     void Awake()
     {
@@ -13,16 +17,32 @@ public class NPC_Handler : MonoBehaviour, I_Interactable
 
     public void ShowInteractionOption()
     {
-        if (canvasInstance == null) canvasInstance = Instantiate(interactCanvas, transform.position, Quaternion.identity);
-        else canvasInstance.SetActive(true);
+        if (canvasInstance == null)
+        {
+            canvasInstance = Instantiate(interactCanvas, transform.position, Quaternion.identity);
+            dialogue = canvasInstance.GetComponentInChildren<TextMeshProUGUI>();
+        }
+        else
+        {
+            canvasInstance.SetActive(true);
+            dialogue.text = "Tab to interact";
+        }
     }
 
     public void HideInteractionOption()
     {
-        canvasInstance.SetActive(false);
+
+        dialogue.text = npcData.Goodbye;
+        Invoke("HideandReset", 2f);
     }
     public void Interact()
     {
+        if (dialogue != null) dialogue.text = npcData.Greeting;
+    }
 
+    private void HideandReset()
+    {
+        canvasInstance.SetActive(false);
+        dialogue.text = npcData.Greeting;
     }
 }
--- SummonSupport/Assets/Scripts/Quest&NPC/NPC_SO.cs
+++ SummonSupport/Assets/Scripts/Quest&NPC/NPC_SO.cs
@@ -5,8 +5,11 @@ using System.Collections.Generic;
 public class NPC_SO : ScriptableObject
 {
     [SerializeField] public NPCName npc_Name = NPCName.Villager;
+    [SerializeField] public string Greeting = "Oh?";
+    [SerializeField] public string Goodbye = "Au revoir...";
+
     [SerializeField] public Dictionary<string, string> PlayerToNPC = new Dictionary<string, string>();
-    [SerializeField] public Dictionary<string, List<string>> NPCToPlayer= new Dictionary<string, List<string>>();
+    [SerializeField] public Dictionary<string, List<string>> NPCToPlayer = new Dictionary<string, List<string>>();
     [SerializeField] public QuestName GivesQuest = QuestName.None;
     [SerializeField] public int XpReward = 0;
     [SerializeField] public int GoldReward = 0;
@@ -16,19 +19,20 @@ public class NPC_SO : ScriptableObject
 
     public List<string> GetPlayerDialogueOptions(string NPCWords)
     {
-        if(NPCToPlayer.TryGetValue(NPCWords, out List<string> playerResponseList)) return playerResponseList;
+        if (NPCToPlayer.TryGetValue(NPCWords, out List<string> playerResponseList)) return playerResponseList;
         else
-        {Logging.Error($"The Player has nothing to say to to {NPCWords}"); //throw new Exception("Player has nothing to say".);
-        return new List<string>{"...", "...."};
+        {
+            Logging.Error($"The Player has nothing to say to to {NPCWords}"); //throw new Exception("Player has nothing to say".);
+            return new List<string> { "...", "...." };
         }
     }
     public string GetNPCResponseToPlayer(string playerWords)
     {
-        if(PlayerToNPC.TryGetValue(playerWords, out string NPCWords)) return NPCWords;
+        if (PlayerToNPC.TryGetValue(playerWords, out string NPCWords)) return NPCWords;
         else
         {
             Logging.Error($"The NPC has nothing to say to to {playerWords}"); //throw new Exception("Player has nothing to say".);
-        return "...";
+            return "...";
         }
     }
 
