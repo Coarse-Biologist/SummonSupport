474a4938292ee4324a75aaa9b077815481451805
Coarse-Biologist <168189263+Coarse-Biologist@users.noreply.github.com>
2025-12-14 17:38:29 -0800

    Start scene kind of complete

--- SummonSupport/Assets/Scripts/SummonSupportLightEffects.cs
+++ SummonSupport/Assets/Scripts/SummonSupportLightEffects.cs
@@ -1,18 +1,47 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class SummonSupportLightEffects : MonoBehaviour
 {
     [field: SerializeField] new public Light light;
+    [field: SerializeField] public int flashStrength = 10000;
+    private WaitForSeconds waitTime = new WaitForSeconds(.05f);
+    public bool flashing = false;
+    public float flashDelay = 5f;
+    public float counter = 0f;
+
 
     void Start()
     {
-        InvokeRepeating("FlashOfLightning", 5f, 5f);
+        InvokeRepeating("StartFlashOfLightningCoroutine", 1f, 1f);
+    }
+    private void StartFlashOfLightningCoroutine()
+    {
+        counter += 1f;
+        if (flashing) return;
+        if (flashDelay > counter) return;
+
+        else
+        {
+            StartCoroutine(FlashOfLightning());
+            counter = 0;
+        }
     }
 
-    private void FlashOfLightning()
+    private IEnumerator FlashOfLightning()
     {
-        light.intensity *= 100;
-        Invoke("DimLight", .2f);
+        flashing = true;
+        float flashDuration = UnityEngine.Random.Range(.2f, 1f);
+        while (flashDuration > 0)
+        {
+            light.intensity *= flashDuration * flashStrength * Math.Abs((float)Math.Cos(flashDuration));
+            yield return waitTime;
+            flashDuration -= .05f;
+        }
+        light.intensity = 1;
+        flashDelay = UnityEngine.Random.Range(5f, 11f);
+        flashing = false;
     }
 
     private void DimLight()
